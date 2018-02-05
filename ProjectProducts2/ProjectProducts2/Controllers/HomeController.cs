0c723874cf34137c841c07cc78c385817df363f6
Dannacus <dannacus@live.com>
2018-02-05 14:51:40 -0500

    Initial Commit

---
+++ ProjectProducts2/ProjectProducts2/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjectProducts2.Models;
+
+namespace ProjectProducts2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<Product> aListOfProducts = new List<Product>();
+
+            string[] names = System.IO.File.ReadAllLines(@"C:\Users\Dannacus\OneDrive - Alfred State College\Documents\Fall2017\Programming 1\Products.txt");
+            int[] id = new int[names.Length];
+            for (int index = 0; index < names.Length; index++)
+            {
+                string[] tokens = names[index].Split(',');
+                Product aProduct1 = new Product();
+                aProduct1.ProductId = Convert.ToInt32(tokens[0]);
+                aProduct1.ProductName = tokens[1];
+                aProduct1.UnitPrice = tokens[2];
+                aProduct1.SupplierId = Convert.ToInt32(tokens[3]);
+                aProduct1.CompanyId = Convert.ToInt32(tokens[4]);
+
+                
+                aListOfProducts.Add(aProduct1);
+            }
+
+            ViewBag.ProductList = aListOfProducts;
+
+            
+            return View();
+            
+        }
+    }
+}
---
+++ ProjectProducts2/ProjectProducts2/Models/Products.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectProducts2.Models
+{
+    public class Product
+    {
+        private int productId = -1;
+        private string productName = "n/a";
+        private string unitPrice = "$999,999.99";
+        private int supplierId = -1;
+        private int companyId = -1;
+
+        // These are accessor methods aka Properties
+        // They are also called gets and sets or
+        // getters and setters
+
+        public int ProductId
+        {
+            // this gets info from the object
+            get
+            {
+                return this.productId;
+            }
+            set
+            {
+                this.productId = value;
+            }
+            // Typically you can not change your id
+            // so there is no setter for Id
+        }
+        public string ProductName
+        {
+            // this gets info from the object
+            get
+            {
+                return this.productName;
+            }
+            // this changes the object's value
+            set
+            {
+                if (value.Length == 0)
+                {
+                    this.productName = "n/a";
+                }
+                else
+                {
+                    this.productName = value;
+                }
+
+            }
+        }
+
+        public string UnitPrice
+        {
+            // this gets info from the object
+            get
+            {
+                return this.unitPrice;
+            }
+            // this changes the object's value
+            set
+            {
+                this.unitPrice = value;
+            }
+        }
+
+        public int SupplierId
+        {
+            // this gets info from the object
+            get
+            {
+                return this.supplierId;
+            }
+            // this changes the object's value
+            set
+            {
+                this.supplierId = value;
+            }
+        }
+
+        public int CompanyId
+        {
+            // this gets info from the object
+            get
+            {
+                return this.companyId;
+            }
+            // this changes the object's value
+            set
+            {
+                this.companyId = value;
+            }
+        }
+
+        // Start of Constructors
+
+        //This is the empty constructor
+        public Product() { }
+
+
+        public Product(int anId, string aProduct, string aPrice, int aSupplier, int aCompany)
+        {
+            this.productId = anId;
+            this.productName = aProduct;
+            this.unitPrice = aPrice;
+            this.supplierId = aSupplier;
+            this.companyId = aCompany;
+
+        }
+
+        public override string ToString()
+        {
+            string aMessage = "";
+            aMessage = aMessage + "Product Id: " + this.ProductId + "\n";
+            aMessage = aMessage + "Product Name: " + this.ProductName + "\n";
+            aMessage = aMessage + "Unit Price: " + this.UnitPrice + "\n";
+            aMessage = aMessage + "Supplier Id: " + this.SupplierId + "\n";
+            aMessage = aMessage + "Company Id: " + this.CompanyId + "\n";
+            return aMessage;
+        }
+    }
+}
